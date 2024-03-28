16e2afc6544e9f741d5ddb7ae37e5b452091fda9
Mathias Magnusson <mathias@magnusson.space>
2024-03-28 01:18:37 +0100

    consul: Open ports and enable acl

--- profiles/consul.nix
+++ profiles/consul.nix
@@ -21,9 +21,15 @@ in
       server = true;
       bootstrap_expect = 1;
       client_addr = "0.0.0.0";
+      acl = {
+        enabled = true;
+        default_policy = "deny";
+      };
     };
     extraConfigFiles = [ config.age.secrets.consul-gossip-key.path ];
     interface.advertise = device;
     interface.bind = device;
   };
+
+  networking.firewall.allowedTCPPorts = [ 8600 8500 8501 8502 8503 8300 8301 8302 ];
 }
