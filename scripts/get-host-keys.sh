2a07f2e94016521b4e48f71299f8318f7e23b393
Mathias Magnusson <mathias@magnusson.space>
2024-04-02 23:51:33 +0200

    fix host key persistance
    The ssh daemon refused to use the ed25519 host key since it was world readable.
    Not sure if appending root@$host to the public key does anything but that is how
    sshd generates them.

--- scripts/get-host-keys.sh
+++ scripts/get-host-keys.sh
@@ -9,7 +9,8 @@ if [ -f "$encrypted_host_key_path" ]; then
     mkdir -p ./etc/ssh
 
     age -d -i "$AGE_IDENTITY" -o ./etc/ssh/ssh_host_ed25519_key "$encrypted_host_key_path"
+    chmod 600 ./etc/ssh/ssh_host_ed25519_key
 
     public_key=$(awk -F'"' "/^\s*$host =/{print \$2}" "$secrets/secrets.nix")
-    echo "$public_key" > ./etc/ssh/ssh_host_ed25519_key.pub
+    echo "$public_key root@$host" > ./etc/ssh/ssh_host_ed25519_key.pub
 fi
