264cf91f0855d67a6e5745df20bb661b336b0333
Mathias Magnusson <mathias@magnusson.space>
2024-04-07 01:47:47 +0200

    refactor terraform configuration

---
+++ aws-ses.tf
@@ -0,0 +1,42 @@
+resource "aws_ses_domain_identity" "datasektionen" {
+  domain = data.cloudflare_zone.datasektionen.name
+}
+
+resource "cloudflare_record" "datasektionen_ses_verification" {
+  name    = "_amazonses"
+  type    = "TXT"
+  zone_id = data.cloudflare_zone.datasektionen.id
+  value   = aws_ses_domain_identity.datasektionen.verification_token
+}
+
+resource "aws_ses_domain_dkim" "datasektionen" {
+  domain = data.cloudflare_zone.datasektionen.name
+}
+
+resource "cloudflare_record" "datasektionen_ses_dkim" {
+  count   = 3
+  name    = "${aws_ses_domain_dkim.datasektionen.dkim_tokens[count.index]}._domainkey"
+  type    = "CNAME"
+  zone_id = data.cloudflare_zone.datasektionen.id
+  value   = "${aws_ses_domain_dkim.datasektionen.dkim_tokens[count.index]}.dkim.amazonses.com"
+}
+
+resource "aws_ses_domain_mail_from" "datasektionen" {
+  domain           = data.cloudflare_zone.datasektionen.name
+  mail_from_domain = "sesmail.${data.cloudflare_zone.datasektionen.name}"
+}
+
+resource "cloudflare_record" "datasektionen_mail_from_mx" {
+  name     = aws_ses_domain_mail_from.datasektionen.mail_from_domain
+  type     = "MX"
+  zone_id  = data.cloudflare_zone.datasektionen.id
+  value    = "feedback-smtp.${local.aws_region}.amazonses.com"
+  priority = 10
+}
+
+resource "cloudflare_record" "datasektionen_mail_from_spf" {
+  name    = aws_ses_domain_mail_from.datasektionen.mail_from_domain
+  type    = "TXT"
+  zone_id = data.cloudflare_zone.datasektionen.id
+  value   = "v=spf1 include:amazonses.com -all"
+}
---
+++ hosts.tf
@@ -0,0 +1,80 @@
+resource "hcloud_server" "artemis" {
+  name        = "artemis"
+  image       = "debian-12"
+  server_type = "cx11"
+  ssh_keys    = [hcloud_ssh_key.bootstrap.id]
+}
+
+resource "hcloud_server" "cluster_servers" {
+  for_each    = toset(["zeus", "poseidon", "hades"])
+  name        = each.key
+  image       = "debian-12"
+  server_type = "cx11"
+  ssh_keys    = [hcloud_ssh_key.bootstrap.id]
+}
+
+resource "hcloud_server" "cluster_clients" {
+  for_each    = toset(["ares"])
+  name        = each.key
+  image       = "debian-12"
+  server_type = "cx11"
+  ssh_keys    = [hcloud_ssh_key.bootstrap.id]
+}
+
+locals {
+  all_hosts = merge(
+    { "artemis" = { instance = hcloud_server.artemis, role = "" } },
+    { for name, instance in hcloud_server.cluster_servers: name => { instance = instance, role = "server" } },
+    { for name, instance in hcloud_server.cluster_clients: name => { instance = instance, role = "client" } },
+  )
+}
+
+module "nixos_install" {
+  for_each = local.all_hosts
+
+  source                 = "github.com/nix-community/nixos-anywhere//terraform/all-in-one"
+  nixos_system_attr      = ".#nixosConfigurations.${each.key}.config.system.build.toplevel"
+  nixos_partitioner_attr = ".#nixosConfigurations.${each.key}.config.system.build.diskoScript"
+
+  target_host = each.value.instance.ipv4_address
+  instance_id = each.value.instance.id
+
+  # this being marked as sensitive hides all output from nixos-anywhere, but that does not print the private key so this is fine
+  install_ssh_key = nonsensitive(sshkey_ed25519_key_pair.bootstrap.private_key_pem)
+  install_user    = "root"
+
+  target_user = var.ssh_user
+
+  extra_files_script = "${path.module}/scripts/get_new_host_files.sh"
+  extra_environment = {
+    host = each.key,
+    role = each.value.role,
+  }
+}
+
+resource "cloudflare_record" "server_name" {
+  for_each = local.all_hosts
+
+  name    = each.key
+  type    = "A"
+  zone_id = data.cloudflare_zone.betasektionen.id
+  value   = each.value.instance.ipv4_address
+}
+
+resource "cloudflare_record" "server_wildcard" {
+  for_each = local.all_hosts
+
+  name    = "*.${each.key}"
+  type    = "A"
+  zone_id = data.cloudflare_zone.betasektionen.id
+  value   = each.value.instance.ipv4_address
+}
+
+resource "sshkey_ed25519_key_pair" "bootstrap" {
+  comment = "dsekt-infra-boostrap"
+}
+
+resource "hcloud_ssh_key" "bootstrap" {
+  name       = "dsekt-infra-bootstrap"
+  public_key = sshkey_ed25519_key_pair.bootstrap.public_key
+}
