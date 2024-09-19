092907f497b2f9c881e80065c334b7b60a8f403e
Mathias Magnusson <mathias@magnusson.space>
2024-09-19 22:22:14 +0200

    set up a github.com/plankanban/planka instance

--- hosts/ares.nix
+++ hosts/ares.nix
@@ -18,17 +18,27 @@ in
     traefik
   ];
 
-  services.nomad.settings.client.host_volume = lib.listToAttrs (
-    lib.forEach mattermost-dirs (x: {
-      name = "mattermost/${x}";
-      value = {
-        path = "/var/lib/mattermost/${x}";
-      };
-    })
-  );
-  systemd.tmpfiles.rules = lib.forEach mattermost-dirs (
-    x: "d /var/lib/mattermost/${x} 0750 2000 2000"
-  );
+  services.nomad.settings.client.host_volume = lib.listToAttrs
+    (
+      lib.forEach mattermost-dirs (x: {
+        name = "mattermost/${x}";
+        value = {
+          path = "/var/lib/mattermost/${x}";
+        };
+      })
+    ) // {
+    "planka/avatars" = { path = "/var/lib/planka/avatars"; };
+    "planka/backgrounds" = { path = "/var/lib/planka/backgrounds"; };
+    "planka/attachments" = { path = "/var/lib/planka/attachments"; };
+  };
+  systemd.tmpfiles.rules = lib.forEach mattermost-dirs
+    (
+      x: "d /var/lib/mattermost/${x} 0750 2000 2000"
+    ) ++ [
+    "d /var/lib/planka/avatars 0750 1000 1000"
+    "d /var/lib/planka/backgrounds 0750 1000 1000"
+    "d /var/lib/planka/attachments 0750 1000 1000"
+  ];
 
   # Change this if you want to lose all data on this machine!
   system.stateVersion = "23.11";
