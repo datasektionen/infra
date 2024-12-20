7e351b08cc4cfcc8e4824015dde5f4720abe15e6
Mathias Magnusson <mathias@magnusson.space>
2024-12-20 20:20:34 +0100

    increase postgresql max connections to 400
    I think default is 100, which we're sometimes reaching now it seems
    like. At least twenty has been erroring and it seems like that's why.
    Found out you can see current connection count by database using `select
    datname, numbackends from pg_stat_database;` and it seems we have a
    reasonable number of connections per system yet the total is circa 80.

--- profiles/postgres.nix
+++ profiles/postgres.nix
@@ -8,6 +8,9 @@
       host all all ${config.dsekt.addresses.subnet} md5
       host all all 172.16.0.0/12 md5 # allow connections from docker (well, all class C private networks)
     '';
+    settings = {
+      max_connections = 400;
+    };
   };
   networking.firewall.allowedTCPPorts = [ 5432 ];
 }
