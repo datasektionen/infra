aa8d08a2f13ca789ebc84454e9eb68b53fd75ce0
Viktor Ekby <viktoe@datasektionen.se>
2026-01-15 20:08:22 +0100

    update nix 24.11 -> 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configurations for Konglig Datasektionen's servers";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
