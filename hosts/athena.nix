c568edec9a309bbe2be564c56f5ce75e39805a30
Viktor Ekby <viktoe@datasektionen.se>
2026-06-25 20:42:13 +0200

    refactor: move all volumes to ares

--- hosts/athena.nix
+++ hosts/athena.nix
@@ -7,31 +7,6 @@
     traefik
   ];
 
-  dsekt.nomad.volumes.host.vault = {
-    userId = 0; # vaultwarden runs as root
-    dirs = [ "data" ];
-  };
-
-  dsekt.nomad.volumes.host.planka = {
-    userId = 1000;
-    dirs = [
-      "user-avatars"
-      "background-images"
-      "favicons"
-      "attachments"
-    ];
-  };
-
-  dsekt.nomad.volumes.host.prometheus = {
-    userId = 65534; # Runs as nobody?
-    dirs = [ "data" ];
-  };
-
-  dsekt.nomad.volumes.host.apollo = {
-    userId = 65533; # Runs as nobody?
-    dirs = [ "data" ];
-  };
-
   # Change this if you want to lose all data on this machine!
   system.stateVersion = "24.11";
 }
